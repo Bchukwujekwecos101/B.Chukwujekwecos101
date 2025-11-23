714c4d1741681b32fc5ed924e216ce546a45a434
Bchukwujekwecos101 <b.chukwujekwe@pau.edu.ng>
2025-11-24 00:36:35 +0100

    (Better late than never /:)

---
+++ week-8/practise_12/target/debug/deps/practise_12.d
@@ -0,0 +1,5 @@
+C:\Users\user\Documents\B.Chukwujekwecos101\week-8\practise_12\target\debug\deps\practise_12.d: src\main.rs
+
+C:\Users\user\Documents\B.Chukwujekwecos101\week-8\practise_12\target\debug\deps\practise_12.exe: src\main.rs
+
+src\main.rs:
---
+++ week-8/practise_12/target/debug/practise_12.d
@@ -0,0 +1,1 @@
+C:\Users\user\Documents\B.Chukwujekwecos101\week-8\practise_12\target\debug\practise_12.exe: C:\Users\user\Documents\B.Chukwujekwecos101\week-8\practise_12\src\main.rs
